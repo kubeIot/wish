299db624282a7956f92ac06115065ec950c25fe0
Marek Hrvol <mhrvol@seznam.cz>
2017-03-18 11:13:35 +0100

    deleted unnecessary files

--- app/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'dashboard',
-    templateUrl: '../html/dashboard.component.html',
-    styleUrls: ['css/app.component.css','../css/device.component.css', 'css/dashboard.component.css']
-})
-
-export class DashboardComponent { }
